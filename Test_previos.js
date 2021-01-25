91b3589626e4617585b5edba69f4882d59c19d32
lazarocantera <77278596+lazarocantera@users.noreply.github.com>
2021-01-25 09:14:55 +0100

    test

---
+++ Test_previos.js
@@ -0,0 +1,8 @@
+for (let i = 0; i<3; i++) {
+    console.log(`Esta es la iteración ${i}`);
+}
+const mesesAño = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 
+'Julio', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+for (let i=0; i<12; i++){
+    console.log(mesesAño[i]);
+}
