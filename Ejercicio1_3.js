d619489596446099a35a2226f6594d903ae50c78
lazarocantera <77278596+lazarocantera@users.noreply.github.com>
2021-01-25 08:46:22 +0100

    comparacion

--- Ejercicio1_3.js
+++ Ejercicio1_3.js
@@ -3,7 +3,7 @@ var comparacion1 = data[1] > data[2];
 console.log(comparacion1);
 comparacion1 === true;
 comparacion1 === false;
-console.log("El segundo número es mayor");
+console.log("El segundo número, ", data[2], " es mayor que ", data[1]);
 console.log('Suma= ', data[1] + data[2]);
 console.log('Resta= ', data[1] - data[2]);
 console.log('Multiplicación= ', data[1] * data[2]);
